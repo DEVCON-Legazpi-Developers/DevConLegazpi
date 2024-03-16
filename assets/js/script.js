539dd4530145bb9b21ea7e1d0435cc4aa20812b6
joemar25 <joemar.cardino000@gmail.com>
2024-03-16 21:35:55 +0800

    Iteration 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -210,26 +210,26 @@
    * Porfolio isotope and filter
    */
   window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: ".portfolio-item",
+    let partnersContainer = select(".partners-container");
+    if (partnersContainer) {
+      let partnersIsotope = new Isotope(partnersContainer, {
+        itemSelector: ".partners-item",
         layoutMode: "fitRows",
       });
 
-      let portfolioFilters = select("#portfolio-flters li", true);
+      let partnersFilters = select("#partners-flters li", true);
 
       on(
         "click",
-        "#portfolio-flters li",
+        "#partners-flters li",
         function (e) {
           e.preventDefault();
-          portfolioFilters.forEach(function (el) {
+          partnersFilters.forEach(function (el) {
             el.classList.remove("filter-active");
           });
           this.classList.add("filter-active");
 
-          portfolioIsotope.arrange({
+          partnersIsotope.arrange({
             filter: this.getAttribute("data-filter"),
           });
         },
@@ -239,16 +239,16 @@
   });
 
   /**
-   * Initiate portfolio lightbox
+   * Initiate partners lightbox
    */
-  const portfolioLightbox = GLightbox({
-    selector: ".portfolio-lightbox",
+  const partnersLightbox = GLightbox({
+    selector: ".partners-lightbox",
   });
 
   /**
-   * Portfolio details slider
+   * partners details slider
    */
-  new Swiper(".portfolio-details-slider", {
+  new Swiper(".partners-details-slider", {
     speed: 400,
     autoplay: {
       delay: 5000,
